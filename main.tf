45dca5e3d06192f9824b51ceb4a5e8c8aa53381e
Carlos Solis Salazar <carlos@soysoliscarlos.com>
2023-07-13 12:11:03 -0500

    Resolv error "availability_zone not expected"

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "azurerm_public_ip" "DC-Public-IP" {
   name                = "DCPublicIp"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  availability_zone   = "No-Zone"
+  #availability_zone   = "No-Zone"
   allocation_method   = "Static"
 }
 
